17c08fecc57d62343c6b28870db81c3d83d6e401
hamzaabbasminhas <hamza.abbas.minhas@gmail.com>
2023-11-16 10:34:16 +0100

    Add comments to the code

--- cypress/e2e/src/specs/test_hotel_booking.js
+++ cypress/e2e/src/specs/test_hotel_booking.js
@@ -7,14 +7,14 @@ describe('Test Suite for Hotel Booking App', function()
 {
 
 beforeEach(() => { 
-    //visit the url saved in env before executing every test
+// Visit the URL and reload before each test
     cy.visit('')
-     cy.reload()
+    cy.reload()
 })
 
 let data;
 before(function() {
-
+    // Load test data from fixture file
     cy.fixture('data').then((fdata) => {
         data = fdata
     }
@@ -22,11 +22,14 @@ before(function() {
 });
 
 it('Verify if the tool calculates occupancy correctly with standard input.',function() {
-    
+    // Test steps...
+
+     // Set input values and click calculate button
     homePage.getInputField(0).clear().type(data.TestData1[0])
     homePage.getInputField(1).clear().type(data.TestData1[1])
     homePage.getCalculateOccupancyButton().click();
 
+    // Verify expected results
     homePage.getFreePremiumRoomsField().should('have.text',data.expectedResult[0])
     homePage.getFreeEconomyRoomsField().should('have.text',data.expectedResult[1])
     homePage.getUsagePremiumField().should('have.text',data.expectedResult[2])
@@ -35,10 +38,14 @@ it('Verify if the tool calculates occupancy correctly with standard input.',func
 }  )
 
 it('Verify if excess premium guests are handled correctly.', function(){
+    // Test steps...
+
+     // Set input values and click calculate button
     homePage.getInputField(0).click().type(data.TestData2[0])
     homePage.getInputField(1).click().type(data.TestData2[1])
     homePage.getCalculateOccupancyButton().click();
-
+    
+    // Verify expected results
     homePage.getFreePremiumRoomsField().should('have.text',data.expectedResult2[0])
     homePage.getFreeEconomyRoomsField().should('have.text',data.expectedResult2[1])
     homePage.getUsagePremiumField().should('have.text',data.expectedResult2[2])
@@ -48,10 +55,14 @@ it('Verify if excess premium guests are handled correctly.', function(){
 
 it('Verify if the highest paying economy guests are upgraded to premium when possible', function(){
 
+    // Test steps...
+
+    // Set input values and click calculate button
     homePage.getInputField(0).click().type(data.TestData3[0])
     homePage.getInputField(1).click().type(data.TestData3[1])
     homePage.getCalculateOccupancyButton().click();
 
+    // Verify expected results
     homePage.getFreePremiumRoomsField().should('have.text',data.expectedResult3[0])
     homePage.getFreeEconomyRoomsField().should('have.text',data.expectedResult3[1])
     homePage.getUsagePremiumField().should('have.text',data.expectedResult3[2])
@@ -60,6 +71,9 @@ it('Verify if the highest paying economy guests are upgraded to premium when pos
 
 it('Verify economy customers are not upgraded to premium rooms if economy rooms are available',function(){
 
+    // Test steps...
+
+    // Set input values and click calculate button
     homePage.getInputField(0).click().type(data.TestData4[0])
     homePage.getInputField(1).click().type(data.TestData4[1])
     homePage.getCalculateOccupancyButton().click();
@@ -73,10 +87,14 @@ it('Verify economy customers are not upgraded to premium rooms if economy rooms 
 
 it('Verify that guests willing to pay EUR 100 or more are not allocated to economy rooms', function(){
 
+    // Test steps...
+
+    // Set input values and click calculate button
     homePage.getInputField(0).click().type(data.TestData2[0])
     homePage.getInputField(1).click().type(data.TestData2[1])
     homePage.getCalculateOccupancyButton().click();
 
+    // Verify expected results
     homePage.getFreePremiumRoomsField().should('have.text',data.expectedResult2[0])
     homePage.getFreeEconomyRoomsField().should('have.text',data.expectedResult2[1])
     homePage.getUsagePremiumField().should('have.text',data.expectedResult2[2])
@@ -85,10 +103,16 @@ it('Verify that guests willing to pay EUR 100 or more are not allocated to econo
 })
 
 it('Verify the behavior when no rooms are available', function(){
+
+    // Test steps...
+
+    // Set input values and click calculate button
+
     homePage.getInputField(0).click().type(data.TestData5[0])
     homePage.getInputField(1).click().type(data.TestData5[1])
     homePage.getCalculateOccupancyButton().click();
 
+    // Verify expected results
     homePage.getFreePremiumRoomsField().should('have.text',data.expectedResult5[0])
     homePage.getFreeEconomyRoomsField().should('have.text',data.expectedResult5[1])
     homePage.getUsagePremiumField().should('have.text',data.expectedResult5[2])
